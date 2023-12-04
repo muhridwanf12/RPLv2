fd06e3d7fc5c268d1d267b2557510521e753dbc9
muhridwanf12 <muhridwanf12@gmail.com>
2023-12-04 11:16:30 +0700

    second commit

--- lib/page_kalkulator/tampil_grafik.dart
+++ lib/page_kalkulator/tampil_grafik.dart
@@ -2,35 +2,42 @@ part of '_index.dart';
 
 void grafik(BuildContext context, double x, double y, String judul) {
   showAdaptiveDialog(
+
     context: context,
     builder: (BuildContext context) {
       return Center(
-        child: Container(
-          color: Colors.white,
-          width: 500,
-          height: 500,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(20.0),
-          ),
-          padding: const EdgeInsets.all(10),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisSize: MainAxisSize.max,
-            children: [
-              Text(
-                judul,  //judul grafik
-                style: const TextStyle(
-                  fontSize: 15, 
-                  fontWeight: FontWeight.bold
-                )
-              ),
-
-              //jarak pemisah judul dengan grafik
-              const SizedBox(height: 3),
-
-              //grafik
-
-            ],
+        
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Container(
+            width: 500,
+            height: 500,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(20.0),
+            ),
+            padding: const EdgeInsets.all(10),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                Text(
+                  judul,  //judul grafik
+                  style: const TextStyle(
+                    fontFamily: AutofillHints.name,
+                    fontSize: 17, 
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black
+                  )
+                ),
+          
+                //jarak pemisah judul dengan grafik
+                const SizedBox(height: 3),
+          
+                //grafik
+          
+              ],
+            ),
           ),
         ),
       );
